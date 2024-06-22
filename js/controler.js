b72c8eb6418c559d66209deff98656ed5f14f987
Milky <56239696+MilkyCatsss@users.noreply.github.com>
2024-06-22 23:23:00 +0800

    Add files via upload

---
+++ js/controler.js
@@ -0,0 +1,121 @@
+const wrapper = document.querySelector("#wrapper"),
+mainAudio = document.querySelector("#main-audio"),
+musicName = document.querySelector("#text>.musicTitle"),
+playPauseBtn = document.querySelector(".playIcon"),
+prevBtn = document.querySelector(".prev"),
+nextBtn = document.querySelector(".next"),
+progressBar = document.querySelector(".music_progress_line");
+
+// 控制歌曲列表的索引
+let musicIndex = 1;
+
+window.addEventListener("load", () => {
+    loadMusic(musicIndex);
+})
+
+// 加载歌曲信息
+function loadMusic(indexNum) {
+    musicName.innerText = allMusic[indexNum - 1].name;
+    mainAudio.src = `audio/${allMusic[indexNum - 1].src}.mp3`;
+}
+
+// 播放控制
+playPauseBtn.addEventListener('click', function() {
+    // 检查音频是否已暂停
+    if (mainAudio.paused) {
+        // 如果已暂停，则播放音频
+        mainAudio.play();
+    } else {
+        // 如果正在播放，则暂停音频
+        mainAudio.pause();
+    }
+})
+
+var iconContainer = document.querySelector('.controler');
+let icon = iconContainer.querySelector('.playIcon');
+
+var isPlay = true;
+
+// 图标切换控制
+document.addEventListener('DOMContentLoaded', function() {
+    playPauseBtn.addEventListener('click', function() {
+        if (isPlay) {
+            // 切换到暂停图标
+            icon.classList.remove('icon-bofang');
+            icon.classList.add('icon-ai07');
+            mainAudio.play();
+        } else {
+            // 切换回播放图标
+            icon.classList.remove('icon-ai07');
+            icon.classList.add('icon-bofang');
+            mainAudio.pause();
+        }
+        isPlay = !isPlay; // 切换状态
+    })
+})
+
+// 上一首的函数控制
+function prevMusic() {
+    musicIndex--;
+    musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+    loadMusic(musicIndex);
+    // 切换到暂停图标
+    icon.classList.remove('icon-bofang');
+    icon.classList.add('icon-ai07');
+    mainAudio.play();
+    isPlay = false; // 切换状态
+}
+
+// 下一首的函数控制
+function nextMusic() {
+    musicIndex++;
+    musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+    loadMusic(musicIndex);
+    // 切换回暂停图标
+    icon.classList.remove('icon-bofang');
+    icon.classList.add('icon-ai07');
+    mainAudio.play();
+    isPlay = true; // 切换状态
+}
+
+// 上一首
+prevBtn.addEventListener('click', function() {
+    prevMusic();
+})
+
+// 下一首
+nextBtn.addEventListener('click', function() {
+    nextMusic();
+})
+
+
+// 进度条更新
+mainAudio.addEventListener('timeupdate', function(e) {
+    const currentTime = e.target.currentTime;   // 获取当前播放的进度
+    const duration = e.target.duration; // 获取总的播放长度
+    let progressWidth = (currentTime / duration) * 100; // 获取百分比
+    progressBar.style.width = `${progressWidth}%`;  // 应用百分比
+
+
+    // 获取当前播放进度和播放总进度的容器
+    let musicCurrentTime = document.querySelector('.current');
+    let musicDuration = document.querySelector('.duration');
+    // 更新歌曲总长度的时间
+    mainAudio.addEventListener('loadeddata', function() {
+
+        let audioDuration = mainAudio.duration;
+        let totalMin = Math.floor(audioDuration / 60);
+        let totalSec = Math.floor(audioDuration % 60);
+        if (totalSec < 10) {
+            totalSec = `0${totalSec}`;
+        }
+        musicDuration.innerText = `${totalMin}:${totalSec}`;
+    })
+    // 更新正在播放的时间
+    let currentMin = Math.floor(currentTime / 60);
+        let currentSec = Math.floor(currentTime % 60);
+        if (currentSec < 10) {
+            currentSec = `0${currentSec}`;
+        }
+        musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+})
---
+++ js/musicList.js
@@ -0,0 +1,52 @@
+let allMusic = [
+    {
+        name: "RE Aoharu",
+        img: "EncroachedSky",
+        src: "01_REAoharu"
+    },
+    {
+        name: "EncroachedSky",
+        img: "EncroachedSky",
+        src: "02_EncroachedSky"
+    },
+    {
+        name: "Polyphonic",
+        img: "EncroachedSky",
+        src: "03_Polyphonic"
+    },
+    {
+        name: "KaitenHurricane!!!",
+        img: "EncroachedSky",
+        src: "04_KaitenHurricane!!!"
+    },
+    {
+        name: "System All Green",
+        img: "EncroachedSky",
+        src: "05_SystemAllGreen"
+    },
+    {
+        name: "Final Destination of Ark",
+        img: "EncroachedSky",
+        src: "06_FinalDestinationofArk"
+    },
+    {
+        name: "Responsibility",
+        img: "EncroachedSky",
+        src: "07_Responsibility"
+    },
+    {
+        name: "Memories of Kindness",
+        img: "EncroachedSky",
+        src: "08_MemoriesofKindness"
+    },
+    {
+        name: "Welcome School",
+        img: "EncroachedSky",
+        src: "09_WelcomeSchool"
+    },
+    {
+        name: "Aoharu Band Arrange",
+        img: "EncroachedSky",
+        src: "10_AoharuBandArrange"
+    }
+]
